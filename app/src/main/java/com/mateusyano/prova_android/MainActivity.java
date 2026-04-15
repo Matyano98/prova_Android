7e56b953ddb28b3f7c38a34249fb583c3c464bc0
mateus yano <mattochioyano98@gmail.com>
2026-04-15 20:11:53 -0300

    implementa listagem de produtos com atualizacao automatica

--- app/src/main/java/com/mateusyano/prova_android/MainActivity.java
+++ app/src/main/java/com/mateusyano/prova_android/MainActivity.java
@@ -1,24 +1,57 @@
 package com.mateusyano.prova_android;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listViewProdutos;
+    private ProdutoDatabase db;
+    private List<String> listaExibicao;
+    private ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        db = ProdutoDatabase.getInstancia(this);
+        listViewProdutos = findViewById(R.id.list_produtos);
+        Button btnIrCadastro = findViewById(R.id.btn_voltar_cadastro);
+
+        listaExibicao = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaExibicao);
+        listViewProdutos.setAdapter(adapter);
+
+        btnIrCadastro.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+            startActivity(intent);
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        carregarProdutos();
+    }
+
+    private void carregarProdutos() {
+        List<Produto> listaProdutos = db.produtoDao().listarTodos();
+        listaExibicao.clear();
+
+        for (Produto p : listaProdutos) {
+            String info = p.getNome() + " | Código: " + p.getCodigo() + " | R$ " + String.format("%.2f", p.getPreco());
+            listaExibicao.add(info);
+        }
+
+        adapter.notifyDataSetChanged();
     }
 }
